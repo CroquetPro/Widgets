48217119815473bdb9cce4a9cb1ea5e284e2cbb2
Ben Rothman <croquetpro@gmail.com>
2015-12-07 10:23:55 -0800

    webpack ,react, babel, server all working

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/widget.jsx",
+  output: {
+    path: "./",
+    filename: "bundle.js",
+    devtoolModuleFilenameTemplate: '[resourcePath]',
+    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react']
+        }
+      }
+    ]
+  },
+
+  devtool: 'source-maps',
+  resolve: {
+    extensions: ["", ".js", '.jsx']
+  }
+
+};
